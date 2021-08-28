ca43ab3819d68b360bb766d125b0ef4efe1058fc
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-28 20:34:01 +0530

    log data

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,10 @@ async function run() {
         }
     );
 
+    console.log('###SID###');
+    console.log(reportContent);
+    console.log(headCoverage);
+
     await runStage('publishReport', dataCollector, async (skip) => {
         if (!isReportContentGenerated) {
             skip();
