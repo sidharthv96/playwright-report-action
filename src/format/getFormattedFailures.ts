64fe9eca5269bd280ef4df727e7cc3852ba16128
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-29 00:03:29 +0530

    test

--- src/format/getFormattedFailures.ts
+++ src/format/getFormattedFailures.ts
@@ -3,7 +3,8 @@ import stripAnsi from 'strip-ansi';
 import { JsonReport } from '../typings/JsonReport';
 
 export const getFailureDetails = (report: JsonReport): string =>
-    report.testResults && report.testResults.length > 0
+    report.testResults &&
+    report.testResults.some(({ message }) => message.length > 0)
         ? '\n```bash\n' +
           report.testResults
               ?.map(({ message }) => stripAnsi(message))
