54034d80d61437e9cdf5e309e8a92a4c40f76a9e
Sidharth Vinod <sidharthv96@gmail.com>
2021-08-29 00:39:04 +0530

    test

--- esbuild.cjs
+++ esbuild.cjs
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { build } = require('esbuild');
-const { markdownPlugin } = require('esbuild-plugin-markdown');
 build({
     bundle: true,
     minify: true,
     platform: 'node',
     entryPoints: ['src/index.ts'],
     outfile: 'dist/index.js',
-    plugins: [markdownPlugin()],
+    loader: {
+        md: 'text',
+    },
 }).catch(() => process.exit(1));
