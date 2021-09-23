399b8f664dc2f0a880f614c3575f1b746f74b79f
Sidharth Vinod <sidharthv96@gmail.com>
2021-09-23 10:05:24 +0530

    Build

--- src/stages/runTest.ts
+++ src/stages/runTest.ts
@@ -1,10 +1,11 @@
 import { exec } from '@actions/exec';
 
+import { joinPaths } from '../utils/joinPaths';
+
 export const runTest = async (
     testCommand: string,
     workingDirectory?: string
 ) => {
-    console.log(testCommand);
     console.log('pwd');
     await exec('pwd', undefined, {
         cwd: workingDirectory,
@@ -18,14 +19,13 @@ export const runTest = async (
         cwd: workingDirectory,
     });
 
-    console.log(
-        await exec('npx playwright test --help', [], {
-            // cwd: workingDirectory,
-        })
-    );
-    console.log(
-        await exec(testCommand, [], {
-            // cwd: workingDirectory,
-        })
-    );
+    await exec('npx playwright test --help', [], {
+        // cwd: workingDirectory,
+    });
+
+    console.log(testCommand);
+
+    await exec(testCommand, [], {
+        cwd: joinPaths(workingDirectory, '..'),
+    });
 };
