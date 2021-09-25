c4a59a38c93035a705f325b56026bc3b909ec7b9
Sidharth Vinod <sidharthv96@gmail.com>
2021-09-25 22:22:07 +0530

    Build

--- src/utils/runStage.ts
+++ src/utils/runStage.ts
@@ -15,7 +15,7 @@ export const runStage = async <T, V>(
     action: (skip: () => never) => Promise<T> | T
 ): Promise<StageResult<T>> => {
     const stageKey = `stages.${stage}`;
-        console.log(stageKey);
+    console.log(`::group::${stageKey}`);
 
     dataCollector.info(
         i18n('stages.defaults.begin', {
@@ -53,5 +53,6 @@ export const runStage = async <T, V>(
                 stage: i18n(stageKey),
             })
         );
+        console.log('::endgroup::');
     }
 };
